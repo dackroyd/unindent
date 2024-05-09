b287fdca2ea944540d10fb12361d302733c77a08
David Ackroyd <23301187+dackroyd@users.noreply.github.com>
2024-05-10 00:50:00 +1000

    ✨ Add Simple Case Return Detection
    Detect unnecessary "else" when earlier cases always return in
    the main body of the if/else-if.
    
    More complex cases of nested if/else blocks and other branching
    statements are not handled at this stage.

--- testdata/src/unindent/simple.go
+++ testdata/src/unindent/simple.go
@@ -2,6 +2,125 @@ package unindent
 
 import "strconv"
 
+func Empty() {}
+
+func Ident(x int) int {
+	return x
+}
+
+func IfEmpty(x, y int) int {
+	if x > y {
+		// Continue
+	}
+
+	return y
+}
+
+func MaxOk(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func MaxReturnWithElse(x, y int) int {
+	if x > y {
+		return x
+	} else { // want `^Unnecessary "else": preceding conditions always end in a "return". Remove the "else" wrapping the block of statements$`
+		return y
+	}
+}
+
+func MaxReturnMulti(x, y int) int {
+	if x > y {
+		return x
+	} else if x == y { // want `^Unnecessary "else": preceding conditions always end in a "return". Remove the "else", leaving "if x == y { ... }"$`
+		return x
+	} else { // want `^Unnecessary "else": preceding conditions always end in a "return". Remove the "else" wrapping the block of statements$`
+		return y
+	}
+}
+
+func MaxAssign(x, y int) int {
+	var max int
+
+	if x > y {
+		max = x
+	} else {
+		max = y
+	}
+
+	return max
+}
+
+func MaxAssignReturn(x, y int) int {
+	var max int
+
+	if x > y {
+		max = x
+	} else {
+		// Note: "else" required as "if" doesn't return, but it could be inverted or otherwise simplified if we can detect that in the future
+		return y
+	}
+
+	return max
+}
+
+func MaxAssignReturnMulti(x, y int) int {
+	var max int
+
+	if x > y {
+		max = x
+	} else if x == y {
+		return x
+	} else {
+		// Note: "else" required as "if" doesn't return, but it could be inverted or otherwise simplified if we can detect that in the future
+		return y
+	}
+
+	return max
+}
+
+func MaxReturnAssign(x, y int) int {
+	var max int
+
+	if x > y {
+		return x
+	} else { // want `^Unnecessary "else": preceding conditions always end in a "return". Remove the "else" wrapping the block of statements$`
+		max = y
+	}
+
+	return max
+}
+
+func MaxUpdateIf(x, y int) int {
+	max := x
+
+	if x < y {
+		max = y
+	}
+
+	return max
+}
+
+func Sign(x int) int {
+	sign := 1
+	if x > 0 {
+		return sign
+	} else { // want `^Unnecessary "else": preceding conditions always end in a "return". Remove the "else" wrapping the block of statements$`
+		return -sign
+	}
+}
+
+func SignWithInit(x int) int {
+	if sign := 1; x > 0 {
+		return sign
+	} else { // want `^Unnecessary "else": preceding conditions always end in a "return". Move variable declaration "sign := 1" before the "if", and remove the "else" wrapping the block of statements$`
+		return -sign
+	}
+}
+
 func FizBuzz(x int) string {
 	fizz := "fizz"
 	buzz := "buzz"
@@ -12,7 +131,7 @@ func FizBuzz(x int) string {
 		return fizz
 	} else if x%5 == 0 { // want `^Unnecessary "else": preceding conditions always end in a "return". Remove the "else", leaving "if x%5 == 0 { ... }"$`
 		return buzz
-	} else { // want `^Unnecessary "else": preceding conditions always end in a "return". Remove the "else {}" wrapping the block of statements$`
+	} else { // want `^Unnecessary "else": preceding conditions always end in a "return". Remove the "else" wrapping the block of statements$`
 		return strconv.Itoa(x)
 	}
 }
--- unindent.go
+++ unindent.go
@@ -1,8 +1,12 @@
 package unindent
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
+	"go/format"
+	"go/token"
+	"strings"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -30,18 +34,80 @@ func (analyzer) run(pass *analysis.Pass) (interface{}, error) {
 		return nil, fmt.Errorf("unexpected analyzer type for initial inspection %T", pass.ResultOf[inspect.Analyzer])
 	}
 
-	ins.Preorder([]ast.Node{&ast.IfStmt{}}, func(node ast.Node) {
+	ins.WithStack([]ast.Node{&ast.IfStmt{}}, func(node ast.Node, push bool, stack []ast.Node) (proceed bool) {
 		stmt, ok := node.(*ast.IfStmt)
 		if !ok {
-			return
+			return false
 		}
 
-		analyze(pass, stmt)
+		if push {
+			return analyze(pass, stmt, stack)
+		}
+
+		return true
 	})
 
 	return analysis.Fact(nil), nil
 }
 
-func analyze(pass *analysis.Pass, stmt *ast.IfStmt) {
-	// TODO
+func analyze(pass *analysis.Pass, stmt *ast.IfStmt, _ []ast.Node) bool {
+	if stmt.Else == nil {
+		return false
+	}
+
+	var alwaysReturns bool
+
+	// Where the return statement is declared directly in the "if" block, it should be the last statement
+	// However, Go allows other statements after this, even if they can never be reached
+	for i := len(stmt.Body.List) - 1; i >= 0; i-- {
+		b := stmt.Body.List[i]
+		if _, ok := b.(*ast.ReturnStmt); ok {
+			alwaysReturns = true
+			break
+		}
+	}
+
+	if !alwaysReturns {
+		return false
+	}
+
+	var (
+		args []any
+		msg  strings.Builder
+	)
+
+	msg.WriteString(`Unnecessary "else": preceding conditions always end in a "return". `)
+
+	if stmt.Init == nil {
+		msg.WriteString(`Remove the "else"`)
+	} else {
+		msg.WriteString(`Move variable declaration %q before the "if", and remove the "else"`)
+		args = append(args, mustFormatNode(stmt.Init))
+	}
+
+	switch els := stmt.Else.(type) {
+	case *ast.IfStmt:
+		// "else if <cond>"
+		msg.WriteString(`, leaving "if %s { ... }"`)
+		args = append(args, mustFormatNode(els.Cond))
+	case *ast.BlockStmt:
+		// "else"
+		msg.WriteString(` wrapping the block of statements`)
+	default:
+		pass.Reportf(els.Pos(), `Unexpected ast in "else": %s`, mustFormatNode(stmt.Else))
+		return false
+	}
+
+	pass.Reportf(stmt.Else.Pos(), msg.String(), args...)
+
+	return true
+}
+
+func mustFormatNode(n any) string {
+	var buf bytes.Buffer
+	if err := format.Node(&buf, token.NewFileSet(), n); err != nil {
+		panic(fmt.Errorf("unable to generate formatted node for %+v", n))
+	}
+
+	return buf.String()
 }
